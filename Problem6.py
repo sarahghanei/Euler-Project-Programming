3e23b3f2f21ee1517b95a9c728f90af820a69c66
Sarah Ghanei Ardakani <sarahghanei1997@gmail.com>
2020-08-17 20:38:12 +0430

    Create Problem6.py

---
+++ Problem6.py
@@ -0,0 +1,13 @@
+# # Euler project Problem 6
+# sum_squares = 0
+# square_sum = 0
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+#     sum_squares += pow(i, 2)
+#     # print(sum_squares)
+# square_sum += pow(sum, 2)
+# # print(sum)
+# # print(sum_squares)
+# print(square_sum)
+# print(square_sum - sum_squares)
