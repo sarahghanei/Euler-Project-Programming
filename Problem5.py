3d0a419287b73f676801a702fbfe5050aedce54e
Sarah Ghanei Ardakani <sarahghanei1997@gmail.com>
2020-08-17 20:36:46 +0430

    Create Problem5.py

---
+++ Problem5.py
@@ -0,0 +1,21 @@
+# # Euler project Problem 5
+# def division(n):  # a function checks that number is divisble by 1 to 20
+#     # for i in range(2, 21):
+#     #     if n % i == 0:
+#     #         continue
+#     #     else:
+#     #         return False
+#     # return True
+#     check_list = [9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+#     for i in check_list:
+#         if n % i == 0:
+#             continue
+#         else:
+#             return False
+#     return True
+#
+#
+# number = 2
+# while not division(number):
+#     number += 2
+# print(number)
