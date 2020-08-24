63cce73b7cbc66874dc582ccbcc55827477f85c0
Sarah Ghanei Ardakani <sarahghanei1997@gmail.com>
2020-08-24 11:52:53 +0430

    Create Problem20.py

---
+++ Problem20.py
@@ -0,0 +1,15 @@
+# # Euler project Problem 20
+# def factorial(num):
+#     result=1
+#     while num > 0:
+#        result*=num
+#        num-=1
+#     return result
+# def sum_digits(n):
+#     sum_digit = 0
+#     while n > 0:
+#         sum_digit += int(n % 10)
+#         n = n // 10
+#         n = int(n)
+#     return sum_digit
+# print(sum_digits(factorial(100)))
