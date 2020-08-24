92751fc4f838eb31323ccad5535c6dd417f8b530
Sarah Ghanei Ardakani <sarahghanei1997@gmail.com>
2020-08-24 11:53:20 +0430

    Create Problem14.py

---
+++ Problem14.py
@@ -0,0 +1,17 @@
+# # Euler project Problem 16
+# def pow(n):
+#     result = 2
+#     for i in range(1, n):
+#         result *= 2
+#     return result
+#
+#
+# def sum_digits(n):
+#     sum_digit = 0
+#     while n > 0:
+#         sum_digit += int(n % 10)
+#         n = n // 10
+#         n = int(n)
+#     return sum_digit
+#
+#
